ff5a8e372fe571022a8f0ad9d494d3271d1d78d7
Hebatullah Abdelazem <hebatullah.abdelazem@gizasystems.com>
2023-10-01 13:07:46 +0300

    insertion sort implementation

---
+++ InsertionSort/InsertionAlgo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertionSort
+{
+    /// <summary>
+    /// 1- unsorted=[],key=0,i,j;
+    /// 2- read unsorted
+    /// 3- for i=1, forward i< unsorted.Length
+    ///   3.1- key=unsorted[i];
+    ///   3.2- for j=i-1 backward j>=0
+    ///      3.2.1- if key<unsorted[j] then unsorted[j+1]=unsorted[j]
+    ///      3.2.2- else break this loop
+    ///   3.3- unsorted[j+1]=key
+    /// 4- return unsorted
+    /// </summary>
+    public class InsertionAlgo
+    {    
+        public static List<int> InsertionSort(List<int> unsorted)
+        {
+            int Key,j,i;
+            for(i=1;i<unsorted.Count; i++)
+            {
+                Key = unsorted[i];
+                for( j=i-1;j>=0; j--)
+                {
+                    if (Key < unsorted[j]) unsorted[j + 1] = unsorted[j];
+                    else break;
+                }
+                unsorted[j + 1] = Key;
+
+            }
+            return unsorted;
+
+        }
+    }
+}
---
+++ InsertionSort/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace InsertionSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sortedList = InsertionAlgo.InsertionSort(new List<int> { 1, 5, 20, 2, 7 });
+            Console.WriteLine(string.Join(",", sortedList));
+        }
+    }
+}
