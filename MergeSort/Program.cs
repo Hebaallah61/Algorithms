95ac21f94c8976e5c53332efe73d8a5aad852a51
Hebatullah Abdelazem <hebatullah.abdelazem@gizasystems.com>
2023-10-02 11:28:13 +0300

    add Merge Sort project

---
+++ MergeSort/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace MergeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array =new() { 8, 65, 9, 7, 3, 5, 54 };
+
+            Console.WriteLine(String.Join(", ", array));
+            MergeAlgo.MergeSort(array, 0, array.Count - 1);
+            Console.WriteLine(String.Join(", ", array));
+        }
+    }
+}
