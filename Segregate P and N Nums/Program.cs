faaed39e7f2fdf95442d5f24fa4826f26b226710
Hebatullah Abdelazem <hebatullah.abdelazem@gizasystems.com>
2023-10-02 16:40:17 +0300

    add another way of segregate

--- Segregate P and N Nums/Program.cs
+++ Segregate P and N Nums/Program.cs
@@ -4,11 +4,16 @@
     {
         static void Main(string[] args)
         {
-            List<int> array = new() { 9,-3,5,-2,-8,-6,1,3};
+            List<int> array1 = new() { 9,-3,5,-2,-8,-6,1,3};
 
-            Console.WriteLine(String.Join(", ", array));
-            Segregate_P_N.Segregate(array, 0, array.Count - 1);
-            Console.WriteLine(String.Join(", ", array));
+            Console.WriteLine(String.Join(", ", array1));
+            Segregate_P_N.Segregate(array1, 0, array1.Count - 1);
+            Console.WriteLine(String.Join(", ", array1));
+
+            List<int> array2 = new() { -9, -3, 5, -2, -8, -6, 1, 3 };
+            Console.WriteLine(String.Join(", ", array2));
+            List<int> Result=Segregate_Norm_P_N.Segregate(array2);
+            Console.WriteLine(String.Join(", ", Result));
         }
     }
 }
