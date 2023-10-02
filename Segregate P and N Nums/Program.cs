6f10b6d6eee637b19190438779a5d3ba7072510e
Hebatullah Abdelazem <hebatullah.abdelazem@gizasystems.com>
2023-10-02 16:12:12 +0300

    add segregate positive and negative numbers project

---
+++ Segregate P and N Nums/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Segregate_P_and_N_Nums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> array = new() { 9,-3,5,-2,-8,-6,1,3};
+
+            Console.WriteLine(String.Join(", ", array));
+            Segregate_P_N.MergeSort(array, 0, array.Count - 1);
+            Console.WriteLine(String.Join(", ", array));
+        }
+    }
+}
