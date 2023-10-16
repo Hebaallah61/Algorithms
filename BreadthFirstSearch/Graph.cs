2646079926a4d239e53efe12b1df572777dac9d9
Hebatullah Abdelazem <hebatullah.abdelazem@gizasystems.com>
2023-10-16 12:27:02 +0300

    BFS algorithm

---
+++ BreadthFirstSearch/Graph.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreadthFirstSearch
+{
+    public class Graph
+    {
+        private int last_index = 0;
+        public Vertex[] Vertices;
+
+        public Graph(string[] names)
+        {
+            Vertices = new Vertex[names.Length];
+            foreach(string name in names)
+            {
+                Vertices[last_index] = new Vertex();
+                Vertices[last_index].Name = name;
+                last_index++;
+            }
+        }
+        public void AddEdges(int vertexIndexCurrent, int[] targets)
+        {
+            Vertices[vertexIndexCurrent].VertexLink = new Edge[targets.Length];
+            for(int i = 0;i<targets.Length;i++)
+            {
+                Vertices[vertexIndexCurrent].VertexLink[i]=new Edge(Vertices[vertexIndexCurrent],Vertices[targets[i]]);
+            }
+        }
+        public void BFS()
+        {
+            int v=Vertices.Length; 
+            Queue<Vertex> q = new Queue<Vertex>(v);
+            q.Enqueue(Vertices[0]);
+            Vertices[0].Visited = true;
+            Vertex Current;
+
+            while(q.Count > 0)
+            {
+                Current = q.Dequeue();
+                for(int i = 0; i < Current.VertexLink.Length; i++)
+                {
+                    if (Current.VertexLink[i].Destination.Visited == false)
+                    {
+                        q.Enqueue(Current.VertexLink[i].Destination);
+                        Current.VertexLink[i].Destination.Visited = true;
+                        Console.WriteLine(Current.Name + " - "+ Current.VertexLink[i].Destination.Name);
+                    }
+                }
+            }
+
+        }
+     }
+}
---
+++ BreadthFirstSearch/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BreadthFirstSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new (new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I" });
+            g.AddEdges(0, new int[] { 1, 2 });
+            g.AddEdges(1, new int[] { 0, 3, 4 });
+            g.AddEdges(2, new int[] { 0, 3, 5 });
+            g.AddEdges(3, new int[] { 1, 2, 4 });
+            g.AddEdges(4, new int[] { 1, 5 });
+            g.AddEdges(5, new int[] { 2, 3, 4, 7 });
+            g.AddEdges(6, new int[] { 7, 8 });
+            g.AddEdges(7, new int[] { 5, 6, 8 });
+            g.AddEdges(8, new int[] { 6, 7 });
+
+            g.BFS();
+        }
+    }
+}
