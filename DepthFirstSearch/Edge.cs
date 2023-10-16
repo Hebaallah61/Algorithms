b6284447e2b5329b363c3dcf0e4f84c3446cde41
Hebatullah Abdelazem <hebatullah.abdelazem@gizasystems.com>
2023-10-16 13:15:34 +0300

    DFS algorithm

---
+++ DepthFirstSearch/Edge.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DepthFirstSearch
+{
+    public class Edge
+    {
+        public Vertex Source;
+        public Vertex Destination;
+        public double Weight;
+
+        public Edge(Vertex source, Vertex destination, double weight=0)
+        {
+            Source = source;
+            Destination = destination;
+            Weight = weight;
+        }
+    }
+}
